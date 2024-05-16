fa4bdad87c019500527ba67baf59b7574688f8c6
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2024-05-16 14:06:04 +0100

    Added metadata to pages as part of chapter 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,16 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 import SideNav from '@/app/ui/dashboard/sidenav';
 import ExceptionButton from './ui/exception-button';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
